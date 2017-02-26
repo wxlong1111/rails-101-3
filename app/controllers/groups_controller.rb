ca5ceba000a4839c06df109832f4ede446e634b6
wxlong <wxlong1111@163.com>
2017-02-26 20:38:52 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
+    if @group.save
       redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -34,7 +37,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
-    redirect_to groups_path  
+    redirect_to groups_path
   end
 
   private
