2709c2a89cff7c871e5dffe567bf85bcf5e8bf21
wxlong <wxlong1111@163.com>
2017-02-27 23:59:06 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :grouups
-  has_many :posts
+  has_many :grouups_relationships
+  has_many :participated_group, :through => :grouups_relationships, :source => :group
 end
