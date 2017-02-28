ad0dc0283595c98ae474ee6bf5d38e4389567233
wxlong <wxlong1111@163.com>
2017-02-28 21:52:26 +0800

    implement my posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :groups
+  has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
@@ -18,5 +20,5 @@ class User < ApplicationRecord
   def quit!(group)
     participated_groups.delete(group)
   end
-  
+
 end
